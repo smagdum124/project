5170a64ec4d99c1ec6f1a42b8ea12b222769b400
smagdum124 <smagdum124@gmail.com>
2024-05-30 13:15:01 +0530

    first commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,36 +1,35 @@
-  import React from "react";
-  import message from "../assets/message.png"
-  import telegram from "../assets/telegram.png";
-  import twitter from "../assets/twitter.png";
-  import discord from "../assets/discord.png";
-  import logo from "../assets/Group 8763.png";
-  import "./navbar.css";
+import React from "react";
+import message from "../assets/message.png";
+import telegram from "../assets/telegram.png";
+import twitter from "../assets/twitter.png";
+import discord from "../assets/discord.png";
+import logo from "../assets/Group 8763.png";
+import "./navbar.css";
 
-  function NavBar() {
-    return (
-      <div className="header">
-        <a href="#" className="icon">
-          {/* <FaEnvelope /> */}
-          <img src={message} alt="" />
+function NavBar() {
+  return (
+    <div className="header">
+      <a href="#" className="icon">
+        <img src={message} alt="Message" className="hover-effect" />
+      </a>
+      <div className="logo">
+        <a href="#">
+          <img src={logo} alt="Logo" />
         </a>
-        <div className="logo">
-          <a href="#">
-            <img src={logo} alt="Logo" />
-          </a>
-        </div>
-        <div className="social-icons">
-          <a href="#" className="icon">
-            <img src={telegram} alt="" />
-          </a>
-          <a href="#" className="icon">
-            <img src={twitter} alt="" />
-          </a>
-          <a href="#" className="icon">
-            <img src={discord} alt="" />
-          </a>
-        </div>
       </div>
-    );
-  }
+      <div className="social-icons">
+        <a href="#" className="icon">
+          <img src={telegram} alt="Telegram" className="hover-effect" />
+        </a>
+        <a href="#" className="icon">
+          <img src={twitter} alt="Twitter" className="hover-effect" />
+        </a>
+        <a href="#" className="icon">
+          <img src={discord} alt="Discord" className="hover-effect" />
+        </a>
+      </div>
+    </div>
+  );
+}
 
-  export default NavBar;
+export default NavBar;
